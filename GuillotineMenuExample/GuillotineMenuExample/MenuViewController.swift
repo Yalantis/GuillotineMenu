d00a94d9c1a63afdf8e73f6c51ed0da16d963115
Maksym Lazebnyi <mlazebnyi@gmail.com>
2015-10-08 11:12:53 +0200

    updated sample

---
+++ GuillotineMenuExample/GuillotineMenuExample/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  GuillotineMenuExample
+//
+//  Created by Maksym Lazebnyi on 10/8/15.
+//  Copyright © 2015 Yalantis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+}
